799a28769715187c173237c54d3cc46609a24aee
Muhammad Favian Revaldy <favianrevaldy@gmail.com>
2024-08-19 17:34:37 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 // Plan for javascript is turn the website to dark mode
+
+const observer = new IntersectionObserver((entries) =>
+{
+    entries.forEach((entry) =>
+    {
+        console.log(entry)
+        if (entry.isIntersecting)
+        {
+            entry.target.classList.add('show');
+        } else
+        {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hide');
+hiddenElements.forEach((el) => observer.observe(el));
